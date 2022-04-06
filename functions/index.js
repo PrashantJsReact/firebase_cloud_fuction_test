b2f5cdab83d06b5a77550caefb5bdc18bcf31d8e
Prashant <prashant@ginims.com>
2022-04-07 01:56:15 +0530

    work updated

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
 /* eslint-disable object-curly-spacing */
 /* eslint-disable linebreak-style */
 /* eslint-disable quote-props */
@@ -7,7 +12,6 @@
 const axios = require("axios").default;
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-require("firebase-functions");
 const cors = require("cors");
 const express = require("express");
 const app = express();
@@ -33,16 +37,39 @@ exports.createPaymentLink = functions
         data: data.data,
       };
 
-      axios.request(options).then((res) => {
-        resolve(res.data);
-      }).catch((error) => {
-        reject(error.response.data);
-      });
+      axios
+        .request(options)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((error) => {
+          reject(error.response.data);
+        });
     });
   });
 
 // Cashfree Payment Link Webhook
 exports.getPaymentStatus = functions.https.onRequest((request, response) => {
-    console.log(JSON.stringify(request.body));
-    response.send(JSON.stringify(request.body));
+  //   console.log(JSON.stringify(request.body));
+  // Remove promise It is not working here.
+  return new Promise((resolve, reject) => {
+    admin
+      .firestore()
+      .collection("allSubscription")
+      .doc(request.body.data.link_id)
+      .update({
+        createdDate: request.body.event_time,
+        status: request.body.data.link_status,
+        "currPaymentDetails.refNo":
+          request.body.data.order.transaction_id,
+      })
+      .then(
+        (response) => {
+          resolve(response.id); // undefined
+          console.log(response.id); // undefined
+        },
+        (error) => reject(error)
+      );
+  });
+  //   response.send(JSON.stringify(request.body));
 });
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { environment } from 'environments/environment';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -12,7 +13,10 @@ export class WarningAlertComponent implements OnInit {
   paymentResponse: any;
   paymentLink = '';
   myInput = uuidv4();
-  constructor(private functions: AngularFireFunctions) {}
+  constructor(
+    private functions: AngularFireFunctions,
+    private angularFirestore: AngularFirestore
+  ) {}
 
   ngOnInit(): void {}
 
@@ -26,13 +30,13 @@ export class WarningAlertComponent implements OnInit {
       data: {
         // from UI
         customer_details: {
-          customer_phone: '7934949499',
-          customer_email: 'abcbfsdfsef@gmail.com',
-          customer_name: 'ginimsss technology',
+          customer_phone: '7765811317',
+          customer_email: 'prashant@ginims.com',
+          customer_name: 'Prashant',
         },
         // eslint-disable-next-line object-curly-spacing
         link_notify: { send_sms: true, send_email: true },
-        link_amount: 500,
+        link_amount: 5000,
         link_currency: 'INR',
         link_purpose: 'Payment for function',
         link_partial_payments: false,
@@ -42,7 +46,48 @@ export class WarningAlertComponent implements OnInit {
     });
     obs.subscribe((res) => {
       this.paymentResponse = res;
+      //Q If cashfree through error then what will happen
+      this.angularFirestore
+        .collection('allSubscription')
+        .doc(res.link_id)
+        .set({
+          subscriptionId: res.link_id, //Q updated after creating doc
+          clientId: 123, // from our application
+          clientName: 'corpName', // from our application
+          createdDate: '', // updated after payment done
+          createdBy: 'the loggedIn userId', // this will set as admin user
+          status: 'PENDING', // updated after webhook triggered
+          cashfreeInfo: {
+            link_id: res.link_id,
+            paymentLink: res.link_url,
+            link_created_at: res.link_created_at,
+            link_expiry_time: res.link_expiry_time,
+          },
+          currPaymentDetails: {
+            // paymentMethod: 'Cheque',
+            // refNo: 123456,
+            // totalPrice: 13000,
+            // roomServiceFee: 150,
+            // groupServiceFee: 100,
+            // banguetServiceFee: 12,
+            // invoiceAttachmentLink: 'abc.pdf',
+            paymentMethod: 'cashFree',
+            refNo: '', // updated after payment done
+            totalPrice: res.link_amount,
+            roomServiceFee: '', // from our application
+            groupServiceFee: '', // from our application
+            banguetServiceFee: '', // from our application
+            // invoiceAttachmentLink: 'abc.pdf',
+          },
+          currentPlanSpecifications: {
+            subTdAdminNo: '', // from our application
+            subTdUserNo: '', // from our application
+            subEmpAprUserNo: '', // from our application
+            subLinkedHotelNo: '', // from our application
+          },
+        })
+      console.log(this.paymentResponse);
     });
-    this.myInput = uuidv4();
+    // this.myInput = uuidv4();
   }
 }
